901bed0c8a1cd35854dc731d439368c7182dd51f
李阳 <2271378911@qq.com>
2023-03-09 11:16:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/Main.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * ClassName: ${NAME}
+ * Package: com.atguigu
+ * Description:
+ *
+ * @Author 李大仙儿
+ * @Create ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/atguigu/hello.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * ClassName: hello
+ * Package: com.atguigu
+ * Description:
+ *
+ * @Author 李大仙儿
+ * @Create 2023/3/9 11:13
+ * @Version 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
