d99260556d4acac7d5c2e91923d1c7c546da9543
李阳 <2271378911@qq.com>
2023-03-09 11:19:29 +0800

    second commit

--- src/main/java/com/atguigu/hello.java
+++ src/main/java/com/atguigu/hello.java
@@ -12,5 +12,7 @@ package com.atguigu;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
